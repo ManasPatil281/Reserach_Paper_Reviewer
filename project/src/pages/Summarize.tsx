6b45df60e73685d12013275d5c6b0c727abf4f43
Manas Mahendra  Patil <164866372+ManasPatil281@users.noreply.github.com>
2025-02-05 22:25:04 +0530

    Update Summarize.tsx

--- project/src/pages/Summarize.tsx
+++ project/src/pages/Summarize.tsx
@@ -14,7 +14,7 @@ export default function Summarize() {
  
   const [language, setLanguage] = useState('English');
 
-  const API_BASE_URL = ' http://127.0.0.5:8005';
+  const API_BASE_URL = 'https://us-central1-scholar-mate-449005.cloudfunctions.net/ai-detection';
 
   const handleProcess = async () => {
     setLoading(true);
