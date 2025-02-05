c94e58fb0cc49ee1c4228be1f2df902b5f4a0f65
Manas Mahendra  Patil <164866372+ManasPatil281@users.noreply.github.com>
2025-02-05 22:23:46 +0530

    Update GrammarCheck.tsx

--- project/src/pages/GrammarCheck.tsx
+++ project/src/pages/GrammarCheck.tsx
@@ -8,7 +8,7 @@ export default function GrammarCheck() {
   const [loading, setLoading] = useState(false);
 
 
-  const API_BASE_URL = 'http://127.0.0.5:8005';
+  const API_BASE_URL = 'https://us-central1-scholar-mate-449005.cloudfunctions.net/ai-detection';
 
   const handleProcess = async () => {
     setLoading(true);
